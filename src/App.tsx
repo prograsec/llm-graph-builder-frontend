471caf2a46d57d95324b47466873d66f7a2393a9
Usama <prograsec.usama@gmail.com>
2024-11-28 00:19:50 +0500

    Adjust the checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -5,36 +5,50 @@ import CustomGraphViewModal from './components/Graph/CustomGraphViewModal.tsx';
 import { useQuery } from 'react-query';
 import { getSources } from './services/SourcesList.ts';
 
+function toggleCheck(checkboxes: boolean[], index: number) {
+  if (index < 0 || index >= checkboxes.length) return checkboxes;
+  return checkboxes.map((checkbox, i) => (index === i ? !checkbox : checkbox));
+}
+
 const App: React.FC = () => {
   const [graphViewOpen, setGraphViewOpen] = useState<boolean>(false);
+  const [selected, setSelected] = useState<number>(-1);
   const { data: sources, isLoading: sourcesLoading } = useQuery({ queryKey: ['sources-list'], queryFn: getSources });
-  const [selected, setSelected] = useState<boolean[]>([]);
 
   if (!sources) {
     return <p>Loading sources</p>;
   }
 
-  useEffect(() => {
-    if (sources) {
-      setSelected(sources.data.map((_) => false));
-    }
-  }, [sources]);
-  
-
   return (
     <>
-    <table>
-      {sources.data.map((source, i) => <tr>
-        <td>{source.}</td>
-      </tr>)}
-    </table>
-      <CustomGraphViewModal
-        selectedRows={['mvc.pdf']}
-        inspectedName={'mvc.pdf'}
-        open={graphViewOpen}
-        setGraphViewOpen={() => setGraphViewOpen((open) => !open)}
-        viewPoint={'sample'}
-      />
+      <table className='text-left w-full'>
+        <thead>
+          <tr>
+            <th>Selected</th>
+            <th>File Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sources.data.map((source, i) => (
+            <tr key={source.fileName}>
+              <td>
+                <input type='checkbox' checked={selected === i} onClick={() => setSelected(i)} />
+              </td>
+              <td>{source.fileName}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {selected !== -1 && <button onClick={() => setGraphViewOpen(true)}>View Graph</button>}
+      {selected !== -1 && (
+        <CustomGraphViewModal
+          selectedRows={[sources.data[selected].fileName]}
+          inspectedName={sources.data[selected].fileName}
+          open={graphViewOpen}
+          setGraphViewOpen={() => setGraphViewOpen((open) => !open)}
+          viewPoint={'sample'}
+        />
+      )}
     </>
   );
 };
